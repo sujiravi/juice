6a6d2cb3138ee31e37351f5c1ec516f9f371a807
sujiravi <sujitha@freshdesk.com>
2015-07-18 14:38:59 +0530

    initial commit

---
+++ juice/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juice::Application.config.secret_token = '66dde2c7a329a40a784e83fe35c27c2536480883a40e698366f220a1d81f7905575dd378bf4ee584569e2594933f6f4044c88f411ea12c52ad55db050da1e1b7'
